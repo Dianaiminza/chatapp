c06f098b0fd9dc758294002f1567e884b517f2c7
Dianaiminza <dianaiminza.99@gmail.com>
2021-11-08 11:43:33 +0300

    my first commit

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ dotenv.config();
 module.exports={
     PORT: process.env.PORT || 5000,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:Captain@chat.e0iu1.mongodb.net/chat?retryWrites=true&w=majority',
-  
+  JWT_SECRET: process.env.JWT_SECRET || 'captain',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
 
 };
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user.model");
 const generateAuthToken = require("../utils/generateAuthToken");
 const router = express.Router();
