188234f96809a5cbfdd3b72821dd69eb198a64c7
Dianaiminza <dianaiminza.99@gmail.com>
2021-11-08 13:17:48 +0300

    my first comment

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,27 +4,26 @@ const User = require("../models/user.model");
 const privateKey = process.env.JWT_KEY;
 
 const verifyAuthentication = async (req, res, next) => {
-  try{
+  
     const bearerToken = req.headers["authorization"];
     const token = bearerToken.split(" ")[1];
     const decoded = jwt.verify(token, privateKey);
     const user = await User.findById(decoded._id);
-    if (!user) {
-      return res.status(401).json({
-        message: "You're not authorized to access this information.",
-      });
-    }
+    // if (!user) {
+    //   return res.status(401).json({
+    //     message: "You're not authorized to access this information.",
+    //   });
+    // }
 
-    req.user = user;
-    next();
-  } 
-  catch (error) {
-    console.error(error);
-    res.status(401).json({
-      message: "You're not authorized to acess this information.",
-      error: error.message,
-    });
-  }
+    // req.user = user;
+    // next();
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(401).json({
+  //     message: "You're not authorized to acess this information.",
+  //     error: error.message,
+  //   });
+  // }
 };
 
 module.exports = verifyAuthentication;
