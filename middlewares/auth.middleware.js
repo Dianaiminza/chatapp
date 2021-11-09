cb5189740888c87fc475dd8f960ea828e4fdfcad
Dianaiminza <dianaiminza.99@gmail.com>
2021-11-09 09:29:02 +0300

    my first commit

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -9,21 +9,21 @@ const verifyAuthentication = async (req, res, next) => {
     const token = bearerToken.split(" ")[1];
     const decoded = jwt.verify(token, privateKey);
     const user = await User.findById(decoded._id);
-    // if (!user) {
-    //   return res.status(401).json({
-    //     message: "You're not authorized to access this information.",
-    //   });
-    // }
+    if (!user) {
+      return res.status(401).json({
+        message: "You're not authorized to access this information.",
+      });
+    }
 
-    // req.user = user;
-    // next();
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(401).json({
-  //     message: "You're not authorized to acess this information.",
-  //     error: error.message,
-  //   });
-  // }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({
+      message: "You're not authorized to acess this information.",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = verifyAuthentication;
