6f68b9a191456ea7845d9d5ae155755cdbea69f8
Dianaiminza <dianaiminza.99@gmail.com>
2021-11-09 09:37:52 +0300

    my first commit

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,10 +1,8 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
-
 const privateKey = process.env.JWT_KEY;
-
 const verifyAuthentication = async (req, res, next) => {
-  
+  try{
     const bearerToken = req.headers["authorization"];
     const token = bearerToken.split(" ")[1];
     const decoded = jwt.verify(token, privateKey);
