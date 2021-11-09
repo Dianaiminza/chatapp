87aaae8d8725e6c48d11b47e88e60b2b83782bd8
DIANA IMINZA <51244262+Dianaiminza@users.noreply.github.com>
2021-11-09 16:28:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 module.exports={
     PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://dbUser:Captain@chat.e0iu1.mongodb.net/chat?retryWrites=true&w=majority',
-  JWT_SECRET: process.env.JWT_SECRET || 'captain',
-  accessKeyId: process.env.accessKeyId || 'accessKeyId',
-  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
+  MONGODB_URL: process.env.MONGODB_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || '',
+  accessKeyId: process.env.accessKeyId || '',
+  secretAccessKey: process.env.secretAccessKey || '',
 
 };
 
